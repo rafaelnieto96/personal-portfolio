6a3f2342ea6421dcf83654010526d437d48bedb1
Rafael Nieto <radikan.dev@gmail.com>
2023-12-29 16:45:55 +0100

    terminal btns logic

---
+++ script.js
@@ -0,0 +1,8 @@
+// Yellow btn
+const yellowButton = document.querySelector('.yellow');
+const terminalBody = document.getElementById('terminalBody');
+
+yellowButton.addEventListener('click', function () {
+    terminalBody.classList.toggle('minimized');
+});
+// End Yellow btn
